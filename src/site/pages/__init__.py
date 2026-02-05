53ae9e269712684861bd02627b81cc60c8dbb9e5
Cyberpunk 042 <jfm.devops.expert@gmail.com>
2026-02-05 11:50:55 -0500

    refactor: extract base template to templates/base.py (-221 lines)

---
+++ src/site/pages/__init__.py
@@ -0,0 +1,1 @@
+"""Site pages package for modular page generators."""
---
+++ src/site/templates/__init__.py
@@ -0,0 +1,5 @@
+"""Site templates package."""
+
+from .base import render_page, get_base_css, get_auto_refresh_script
+
+__all__ = ["render_page", "get_base_css", "get_auto_refresh_script"]
